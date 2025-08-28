fdf858992c6ec97b3324566b1ac448e987151d74
pefdore <62886055+pefdore@users.noreply.github.com>
2025-08-29 00:39:31 +0200

    fix: support server-side env variables without NEXT_PUBLIC_ prefix

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -7,8 +7,8 @@ export const createClient = () => {
   const cookieStore = cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
@@ -41,7 +41,7 @@ export const createAdminClient = () => {
   const cookieStore = cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
